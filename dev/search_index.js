0d5224dd50754c371ef707d79e4c38dee1df5bf9
Documenter.jl <documenter@juliadocs.github.io>
2024-09-09 20:36:19 +0000

    build based on 36bda5a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PhyloPickMe","category":"page"},{"location":"#PhyloPickMe","page":"Home","title":"PhyloPickMe","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhyloPickMe.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PhyloPickMe]","category":"page"}]
+}
